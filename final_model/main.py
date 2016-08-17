e80b5e488e25fc7f4ab2900ff23cb7d7f4460efe
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-08-17 09:40:47 -0300

    provide the sentence as input instead the all parsed file

--- final_model/main.py
+++ final_model/main.py
@@ -63,7 +63,7 @@ def run():
 
                 # REALIZATION
                 # compute the frequency of the words in the sentence (words that are not part of the mention)
-                parsed = json.load(open(os.path.join(parsed_dir, mention['fname'])))
+                parsed = json.load(open(os.path.join(parsed_dir, mention['fname'])))['sentences'][mention['sentNum']-1]
                 mentions_same_entity = filter(lambda x: x['fname'] == mention['fname'] and x['sentNum'] == mention['sentNum'], mentions)
                 word_freq = preprocessing.word_freq(mentions_same_entity, parsed)
 
