92baaba1a7c06889c1702cc7921aa3e5c47a1c66
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-09-13 03:57:33 -0300

    Titles

---
+++ main/utils/get_titles.py
@@ -0,0 +1,37 @@
+__author__ = 'thiagocastroferreira'
+
+"""
+Author: Thiago Castro Ferreira
+Date: 13/09/2016
+Description:
+    This script aims to create retrieve all possible titles for each entity
+"""
+import json
+import os
+
+# get all the titles possible for an entity
+def get_titles(mention_dir, write_dir):
+    files = os.listdir(mention_dir)
+
+    result = {}
+    for i, fname in enumerate(files):
+        print i, fname, '\r',
+        mentions = json.load(open(os.path.join(mention_dir, fname)))
+
+        for entity in mentions:
+            if entity not in result:
+                result[entity] = []
+            for mention in filter(lambda mention: mention['type'] == 'PROPER', mentions[entity]):
+                titles = mention['titles']
+                result[entity].extend(titles)
+
+    for entity in result:
+        result[entity] = set(result[entity])
+
+    json.dump(result, open(write_dir, 'w'))
+
+if __name__ == '__main__':
+    mention_dir = '/roaming/tcastrof/names/eacl/mentions'
+    write_dir = '/roaming/tcastrof/names/eacl/titles.json'
+
+    get_titles(mention_dir, write_dir)
