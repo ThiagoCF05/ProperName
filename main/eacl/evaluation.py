fe94909b3f6893d4ca32f4ffb85d551c4e70d992
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-09-15 04:16:55 -0300

    Evaluation bayes variation

--- main/eacl/evaluation.py
+++ main/eacl/evaluation.py
@@ -10,12 +10,12 @@ from sklearn.metrics import accuracy_score
 
 if __name__ == '__main__':
     fentities = '/roaming/tcastrof/names/eacl/fentities.json'
-    entities_dir = '/roaming/tcastrof/names/eacl/evaluationV2'
+    entities_dir = '/roaming/tcastrof/names/eacl/evaluationVariation'
     entities = os.listdir(entities_dir)
 
     results = {}
 
-    bayes, siddharthan, deemter  = {}, {}, {}
+    bayes_random, bayes_no_variation, bayes_variation, siddharthan, deemter  = {}, {}, {}, {}, {}
     for _id in entities:
         results[_id] = {}
 
@@ -23,34 +23,54 @@ if __name__ == '__main__':
 
         for fold in evaluation:
             results[_id][fold] = {}
-            if fold not in bayes:
-                bayes[fold] = {'y_real':[], 'y_pred':[], 'string':[], 'jaccard':[]}
+            if fold not in bayes_random:
+                bayes_random[fold] = {'y_real':[], 'y_pred':[], 'string':[], 'jaccard':[]}
+                bayes_no_variation[fold] = {'y_real':[], 'y_pred':[], 'string':[], 'jaccard':[]}
+                bayes_variation[fold] = {'y_real':[], 'y_pred':[], 'string':[], 'jaccard':[]}
                 siddharthan[fold] = {'y_real':[], 'y_pred':[], 'string':[], 'jaccard':[]}
                 deemter[fold] = {'y_real':[], 'y_pred':[], 'string':[], 'jaccard':[]}
 
             for item in evaluation[fold]:
                 string_real = item['real']['reference']
-                string_bayes = item['bayes']['reference'][0][0]
+                string_bayes_random = item['bayes_random']['reference'][0][0]
+                string_bayes_no_variation = item['bayes_no_variation']['reference'][0][0]
+                string_bayes_variation = item['bayes_variation']['reference'][0][0]
                 string_siddharthan = item['siddharthan']['reference']
                 string_deemter = item['deemter']['reference']
 
-                dist_bayes = edit_distance(string_bayes, string_real)
+                dist_bayes_random = edit_distance(string_bayes_random, string_real)
+                dist_bayes_no_variation = edit_distance(string_bayes_no_variation, string_real)
+                dist_bayes_variation = edit_distance(string_bayes_variation, string_real)
                 dist_siddharthan = edit_distance(string_siddharthan, string_real)
                 dist_deemter = edit_distance(string_deemter, string_real)
 
                 tokens_real = set(nltk.word_tokenize(string_real))
-                tokens_bayes = set(nltk.word_tokenize(string_bayes))
+                tokens_bayes_random = set(nltk.word_tokenize(string_bayes_random))
+                tokens_bayes_no_variation = set(nltk.word_tokenize(string_bayes_no_variation))
+                tokens_bayes_variation = set(nltk.word_tokenize(string_bayes_variation))
                 tokens_siddharthan = set(nltk.word_tokenize(string_siddharthan))
                 tokens_deemter = set(nltk.word_tokenize(string_deemter))
 
-                jaccard_bayes = jaccard_distance(tokens_bayes, tokens_real)
+                jaccard_bayes_random = jaccard_distance(tokens_bayes_random, tokens_real)
+                jaccard_bayes_no_variation = jaccard_distance(tokens_bayes_no_variation, tokens_real)
+                jaccard_bayes_variation = jaccard_distance(tokens_bayes_variation, tokens_real)
                 jaccard_siddharthan = jaccard_distance(tokens_siddharthan, tokens_real)
                 jaccard_deemter = jaccard_distance(tokens_deemter, tokens_real)
 
-                bayes[fold]['y_real'].append(item['real']['label'])
-                bayes[fold]['y_pred'].append(item['bayes']['label'][0])
-                bayes[fold]['string'].append(dist_bayes)
-                bayes[fold]['jaccard'].append(jaccard_bayes)
+                bayes_random[fold]['y_real'].append(item['real']['label'])
+                bayes_random[fold]['y_pred'].append(item['bayes_random']['label'][0])
+                bayes_random[fold]['string'].append(dist_bayes_random)
+                bayes_random[fold]['jaccard'].append(jaccard_bayes_random)
+
+                bayes_no_variation[fold]['y_real'].append(item['real']['label'])
+                bayes_no_variation[fold]['y_pred'].append(item['bayes_no_variation']['label'][0])
+                bayes_no_variation[fold]['string'].append(dist_bayes_no_variation)
+                bayes_no_variation[fold]['jaccard'].append(jaccard_bayes_no_variation)
+
+                bayes_variation[fold]['y_real'].append(item['real']['label'])
+                bayes_variation[fold]['y_pred'].append(item['bayes_variation']['label'][0])
+                bayes_variation[fold]['string'].append(dist_bayes_variation)
+                bayes_variation[fold]['jaccard'].append(jaccard_bayes_variation)
 
                 siddharthan[fold]['y_real'].append(item['real']['label'])
                 siddharthan[fold]['y_pred'].append(item['siddharthan']['label'])
@@ -63,7 +83,12 @@ if __name__ == '__main__':
                 deemter[fold]['jaccard'].append(jaccard_deemter)
 
                 result = {
-                    'bayes': {
+                    'bayes_random': {
+                        'label': (item['real']['label'], item['bayes']['label'][0]),
+                        'string': dist_bayes,
+                        'jaccard': jaccard_bayes
+                    },
+                    'bayes_no_variation': {
                         'label': (item['real']['label'], item['bayes']['label'][0]),
                         'string': dist_bayes,
                         'jaccard': jaccard_bayes
