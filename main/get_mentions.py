e4ce58100c0a0bd85af9f41def40073d55a6bbc0
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-09-23 18:26:09 -0300

    Filter entities properly

--- main/get_mentions.py
+++ main/get_mentions.py
@@ -18,17 +18,47 @@ from corpus_builder import get_mentions
 parsed_dir = "/roaming/tcastrof/names/eacl/evaluation/extrinsic/parsed"
 mentions_dir = "/roaming/tcastrof/names/eacl/evaluation/extrinsic/mentions"
 
+# filter entities with more than N mentions and process the mentions
+def filter_entities(N_min, N_max, mention_dir):
+    '''
+    :param N: only entities with more than N mentions should be considered
+    :param mention_dir: directory where the mention files are present
+    :return:
+    '''
+    result = {}
+
+    files = os.listdir(mention_dir)
+    for i, fname in enumerate(files):
+        print i, fname, '\r',
+        mentions = json.load(open(os.path.join(mention_dir, fname)))
+
+        # Exclude this entity from the set
+        entities = filter(lambda x: x != 'http://en.wikipedia.org/wiki/Whoopi_Goldberg', mentions)
+        for entity in entities:
+            if entity not in result:
+                result[entity] = []
+            for mention in filter(lambda mention: mention['type'] == 'PROPER', mentions[entity]):
+                mention['fname'] = fname
+                result[entity].append(mention)
+    if N_max == 0:
+        return dict(map(lambda x: (x, result[x]), filter(lambda e: len(result[e]) >= N_min, result.keys())))
+    else:
+        return dict(map(lambda x: (x, result[x]), filter(lambda e: N_min <= len(result[e]) <= N_max, result.keys())))
+
 if __name__ == '__main__':
     dbpedia = json.load(open(utilities.dbpedia_dir))
     entities = json.load(open(utilities.entities_dir))
+
+    fentities = filter_entities(50, 0, mentions_dir)
 
     if not os.path.exists(mentions_dir):
         os.makedirs(mentions_dir)
 
     nfiles = 0
     notfound = 0
-    for entity in entities:
+    for e in fentities:
         # try:
+        entity = filter(lambda x: x['url'] == e, entities)[0]
         mentions = get_mentions.run(os.path.join(parsed_dir, entity['id']), dbpedia[entity['url']])
 
         if os.path.isfile(os.path.join(mentions_dir, entity['id'])):
