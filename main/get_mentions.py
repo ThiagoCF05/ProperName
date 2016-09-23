467344e196d886b08bc6491c32b2334c04544ef0
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-09-23 18:31:05 -0300

    treat errors

--- main/get_mentions.py
+++ main/get_mentions.py
@@ -59,22 +59,22 @@ if __name__ == '__main__':
     notfound = 0
     for i, e in enumerate(fentities):
         print i, e
-        # try:
-        entity = filter(lambda x: x['url'] == e, entities)[0]
-        mentions = get_mentions.run(os.path.join(parsed_dir, entity['id']), dbpedia[entity['url']])
+        try:
+            entity = filter(lambda x: x['url'] == e, entities)[0]
+            mentions = get_mentions.run(os.path.join(parsed_dir, entity['id']), dbpedia[entity['url']])
 
-        if os.path.isfile(os.path.join(mentions_dir, entity['id'])):
-            j = json.load(open(os.path.join(mentions_dir, entity['id'])))
-            j[entity['url']] = mentions
-        else:
-            j = { entity['url']:mentions }
-        json.dump(j, open(os.path.join(mentions_dir, entity['id']), 'w'), indent=4, separators=(',', ': '))
-        # except ValueError:
-        #     nfiles -= 1
-        #     notfound += 1
-        # except IOError:
-        #     nfiles -= 1
-        #     notfound += 1
-        # except:
-        #     exc_type, exc_value, exc_traceback = sys.exc_info()
-        #     traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2, file=sys.stdout)
+            if os.path.isfile(os.path.join(mentions_dir, entity['id'])):
+                j = json.load(open(os.path.join(mentions_dir, entity['id'])))
+                j[entity['url']] = mentions
+            else:
+                j = { entity['url']:mentions }
+            json.dump(j, open(os.path.join(mentions_dir, entity['id']), 'w'), indent=4, separators=(',', ': '))
+        except ValueError:
+            nfiles -= 1
+            notfound += 1
+        except IOError:
+            nfiles -= 1
+            notfound += 1
+        except:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2, file=sys.stdout)
