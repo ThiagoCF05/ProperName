65c0376c649840d1d126c37be658a318a31c078f
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-09-23 16:32:16 -0300

    Only entities mentioned more than 50 times

--- main/eacl/human_evaluation/get_abstracts.py
+++ main/eacl/human_evaluation/get_abstracts.py
@@ -11,12 +11,40 @@ import json
 import os
 import urllib2
 
-entities_dir = '/roaming/tcastrof/names/eacl/entities.json'
 abstracts_dir = '/roaming/tcastrof/names/eacl/evaluation/extrinsic/abstracts'
+entities_dir = '/roaming/tcastrof/names/eacl/entities.json'
+mention_dir = '/roaming/tcastrof/names/eacl/mentions'
 write_dir = '/roaming/tcastrof/names/eacl/abstracts.json'
 
 dbpedia_url = 'http://dbpedia.org/data/'
 resource_url = 'http://dbpedia.org/resource/'
+
+# filter entities with more than N mentions and process the mentions
+def filter_entities(N_min, N_max, mention_dir):
+    '''
+    :param N: only entities with more than N mentions should be considered
+    :param mention_dir: directory where the mention files are present
+    :return:
+    '''
+    result = {}
+
+    files = os.listdir(mention_dir)
+    for i, fname in enumerate(files):
+        print i, fname, '\r',
+        mentions = json.load(open(os.path.join(mention_dir, fname)))
+
+        # Exclude this entity from the set
+        entities = filter(lambda x: x != 'http://en.wikipedia.org/wiki/Whoopi_Goldberg', mentions)
+        for entity in entities:
+            if entity not in result:
+                result[entity] = []
+            for mention in filter(lambda mention: mention['type'] == 'PROPER', mentions[entity]):
+                mention['fname'] = fname
+                result[entity].append(mention)
+    if N_max == 0:
+        return dict(map(lambda x: (x, result[x]), filter(lambda e: len(result[e]) >= N_min, result.keys())))
+    else:
+        return dict(map(lambda x: (x, result[x]), filter(lambda e: N_min <= len(result[e]) <= N_max, result.keys())))
 
 def get_abstract(entity):
     print entity['url'], '\r',
@@ -39,7 +67,10 @@ if __name__ == '__main__':
     entities = json.load(open(entities_dir))
     abstracts = []
 
-    for entity in entities:
+    fentities = filter_entities(50, mention_dir)
+
+    for e in fentities:
+        entity = filter(lambda x: x['url'] == e, entities)
         url, abstract = get_abstract(entity)
         abstracts.append((url, abstract))
 
