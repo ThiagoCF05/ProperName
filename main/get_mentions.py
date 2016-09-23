50636708e016981826b5fdfc3ef07f6e15175dd6
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-09-23 18:21:58 -0300

    Check mistakes

--- main/get_mentions.py
+++ main/get_mentions.py
@@ -28,21 +28,21 @@ if __name__ == '__main__':
     nfiles = 0
     notfound = 0
     for entity in entities:
-        try:
-            mentions = get_mentions.run(os.path.join(parsed_dir, entity['id']), dbpedia[entity['url']])
+        # try:
+        mentions = get_mentions.run(os.path.join(parsed_dir, entity['id']), dbpedia[entity['url']])
 
-            if os.path.isfile(os.path.join(mentions_dir, entity['id'])):
-                j = json.load(open(os.path.join(mentions_dir, entity['id'])))
-                j[entity['url']] = mentions
-            else:
-                j = { entity['url']:mentions }
-            json.dump(j, open(os.path.join(mentions_dir, entity['id']), 'w'), indent=4, separators=(',', ': '))
-        except ValueError:
-            nfiles -= 1
-            notfound += 1
-        except IOError:
-            nfiles -= 1
-            notfound += 1
-        except:
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2, file=sys.stdout)
+        if os.path.isfile(os.path.join(mentions_dir, entity['id'])):
+            j = json.load(open(os.path.join(mentions_dir, entity['id'])))
+            j[entity['url']] = mentions
+        else:
+            j = { entity['url']:mentions }
+        json.dump(j, open(os.path.join(mentions_dir, entity['id']), 'w'), indent=4, separators=(',', ': '))
+        # except ValueError:
+        #     nfiles -= 1
+        #     notfound += 1
+        # except IOError:
+        #     nfiles -= 1
+        #     notfound += 1
+        # except:
+        #     exc_type, exc_value, exc_traceback = sys.exc_info()
+        #     traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2, file=sys.stdout)
