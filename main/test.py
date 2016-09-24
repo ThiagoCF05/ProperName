f21fac0b38ea5c826335bc91104826de3eb7fd34
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-09-24 10:49:55 -0300

    Evaluation

--- main/test.py
+++ main/test.py
@@ -4,37 +4,43 @@ import json
 import os
 
 mention_dir = '/roaming/tcastrof/names/eacl/mentions'
-write_dir = '/roaming/tcastrof/names/eacl/entities_50.json'
+entities_dir = '/roaming/tcastrof/names/eacl/entities.json'
 
-# filter entities with more than N mentions and process the mentions
-def filter_entities(N_min, N_max, mention_dir):
-    '''
-    :param N: only entities with more than N mentions should be considered
-    :param mention_dir: directory where the mention files are present
-    :return:
-    '''
-    result = {}
-
-    files = os.listdir(mention_dir)
-    for i, fname in enumerate(files):
-        print i, fname, '\r',
-        mentions = json.load(open(os.path.join(mention_dir, fname)))
-
-        # Exclude this entity from the set
-        entities = filter(lambda x: x != 'http://en.wikipedia.org/wiki/Whoopi_Goldberg', mentions)
-        for entity in entities:
-            if entity not in result:
-                result[entity] = []
-            for mention in filter(lambda mention: mention['type'] == 'PROPER', mentions[entity]):
-                mention['fname'] = fname
-                result[entity].append(mention)
-    if N_max == 0:
-        return dict(map(lambda x: (x, result[x]), filter(lambda e: len(result[e]) >= N_min, result.keys())))
-    else:
-        return dict(map(lambda x: (x, result[x]), filter(lambda e: N_min <= len(result[e]) <= N_max, result.keys())))
+entities = ['http://en.wikipedia.org/wiki/Diana_Ross',
+            'http://en.wikipedia.org/wiki/Muammar_Gaddafi',
+            'http://en.wikipedia.org/wiki/Jack_Nicholson',
+            'http://en.wikipedia.org/wiki/Paris_Hilton',
+            'http://en.wikipedia.org/wiki/Anna_Wintour',
+            'http://en.wikipedia.org/wiki/Charles_Manson',
+            'http://en.wikipedia.org/wiki/Lindsay_Lohan',
+            'http://en.wikipedia.org/wiki/Dick_Cheney',
+            'http://en.wikipedia.org/wiki/George_Lucas',
+            'http://en.wikipedia.org/wiki/Clint_Eastwood',
+            'http://en.wikipedia.org/wiki/Britney_Spears',
+            'http://en.wikipedia.org/wiki/Mark_Zuckerberg',
+            'http://en.wikipedia.org/wiki/Anne_Boleyn',
+            'http://en.wikipedia.org/wiki/Kurt_Vonnegut',
+            'http://en.wikipedia.org/wiki/Neville_Chamberlain',
+            'http://en.wikipedia.org/wiki/Kylie_Minogue',
+            'http://en.wikipedia.org/wiki/Elton_John',
+            'http://en.wikipedia.org/wiki/Charles,_Prince_of_Wales',
+            'http://en.wikipedia.org/wiki/Donald_Rumsfeld',
+            'http://en.wikipedia.org/wiki/Magic_Johnson',
+            'http://en.wikipedia.org/wiki/Kobe_Bryant',
+            'http://en.wikipedia.org/wiki/Rick_Santorum',
+            'http://en.wikipedia.org/wiki/Michael_Schumacher',
+            'http://en.wikipedia.org/wiki/Raila_Odinga',
+            'http://en.wikipedia.org/wiki/Mark_Antony']
 
 if __name__ == '__main__':
-    references = filter_entities(50, 0, mention_dir)
+    for entity in entities:
+        print entity
+        for fname in os.listdir(mention_dir):
+            mentions = json.load(open(os.path.join(mention_dir, fname)))
 
-    json.dump(references.keys(), open(write_dir, 'w'), indent=4, separators=(',', ': '))
+            if entity in mentions.keys():
+                f = filter(lambda x: x['type'] == 'PROPER', mentions[entity])
 
+                if len(f) >= 10:
+                    print fname
+        print 10 * '*'
