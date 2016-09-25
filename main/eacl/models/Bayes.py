b89adfefef4a35c22c43de03b8c8bbb0de5f1793
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-09-24 22:03:26 -0300

    Non recursion

--- main/eacl/models/Bayes.py
+++ main/eacl/models/Bayes.py
@@ -198,13 +198,13 @@ class Bayes(object):
             elems.append('+a')
 
         keys = filter(lambda x: x[0] in elems and x[1] == entity, self.clf_content['elem_p'])
-        elems = dict(map(lambda x: (x[0], self.clf_content['elem_p'][x]), keys))
+        aux = dict(map(lambda x: (x[0], self.clf_content['elem_p'][x]), keys))
 
-        for e in ['+t', '+f', '+m', '+l', '+a']:
-            if e not in elems:
-                elems[e] = 0
+        for e in elems:
+            if e not in aux:
+                aux[e] = 0
 
-        elem = sorted(elems.items(), key=operator.itemgetter(1))
+        elem = sorted(aux.items(), key=operator.itemgetter(1))
         print elem
         form = str(form).replace(elem[0][0], '')
         return form
@@ -219,7 +219,7 @@ class Bayes(object):
         while result[result.keys()[0]] == 0 and len(form) > 2:
             form = self._backoff(form, entity)
             result = self._beam_search(names, words, form, entity, word_freq, 1)
-        print 'NEW:', form, result
+            print 'NEW:', form, result
 
         names = []
         for name in result:
