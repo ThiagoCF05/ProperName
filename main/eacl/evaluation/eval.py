16d897b3fb112b77cc11604c7d2a8d673b3242e9
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2017-01-04 11:29:30 -0200

    Fixing the printing of the samples

--- main/eacl/evaluation/eval.py
+++ main/eacl/evaluation/eval.py
@@ -277,7 +277,7 @@ def run(std=True, domain=''):
         print round(t, 6), p
         print 10 * '-'
 
-        print 'Number of samples: ', general_random['string'], general_bayes_no_variation['string']
+        print 'Number of samples: ', len(general_random['string']), len(general_bayes_no_variation['string'])
 
     write_dir = '/roaming/tcastrof/names/eacl/evaluation'
     write_csv(general_random, general_siddharthan, general_deemter, general_bayes_no_variation, general_bayes_variation, write_dir)
